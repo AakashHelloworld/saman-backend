0604060096283705f6eab1bf6245741f5e3e1f43
AakashHelloworldgit config --global user.name AakashHelloworldgit config --global user.name AakashHelloworld <aakashbandhuaryal@gmail.com>
2022-11-27 19:21:03 +0545

    first time rendering

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/api', limiter)
 app.use(express.json( {limit: '10kb'} )); 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: ['http://localhost:3000', 'https://saman-zcx5.onrender.com/']
 }));
 app.use(cookieParser());
 app.use('/api/v1/products', products);
